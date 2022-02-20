9eb09d3585f5c279ed1d33cd665ae2ee24a6aa0f
Tuomas Peltonen <tuomas.j.j.peltonen@gmail.com>
2022-02-20 23:57:05 +0900

    Added new empirical tests of big graphs, documentation, and prototyping

--- src/data_structures/dictionary.py
+++ src/data_structures/dictionary.py
@@ -4,5 +4,13 @@ class Dictionary():
     def __init__(self, object):
         self.size = len(object.keys())
 
-    # def add(self, key, value):
-    #     pass
+    def _check_hash_collision(self):
+        pass
+
+    def add(self, key, value):
+        # Map the key into a hash value 
+        # and then map the hash value into the range of the dictionary array
+        mask = self.size - 1
+        index = hash(key) & mask
+
+        pass
